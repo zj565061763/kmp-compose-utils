a5640996f21ad7235bb143c0d72f8ff7361e377a
zjun <565061763@qq.com>
2025-02-07 15:48:38 +0800

    FVerticalGrid内部改为Row和Column实现

--- lib/src/commonMain/kotlin/com/sd/lib/kmp/compose_utils/VerticalGrid.kt
+++ lib/src/commonMain/kotlin/com/sd/lib/kmp/compose_utils/VerticalGrid.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -41,10 +40,7 @@ fun FVerticalGrid(
   ) {
     val rowCount = safeCount / itemCountPerRow
     repeat(rowCount) { rowIndex ->
-      Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(horizontalSpacing),
-      ) {
+      Row(horizontalArrangement = Arrangement.spacedBy(horizontalSpacing)) {
         repeat(itemCountPerRow) { columnIndex ->
           Box(modifier = Modifier.weight(1f)) {
             val index = (rowIndex * itemCountPerRow) + columnIndex
